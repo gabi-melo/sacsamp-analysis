c309f96b0a5a3b2711a2353071a3905d01aa103c
gabi-melo <gabimelo42@gmail.com>
2025-02-13 19:10:01 -0300

    update analysis

--- MEG/artifact_removal.m
+++ MEG/artifact_removal.m
@@ -1,12 +1,16 @@
 clear all; clc;
 
-main_folder = '/Users/gabimelo/Documents/GitHub/sacsamp-analysis/';
-data_folder = '/Volumes/PortableSSD/SACSAMP/';
-ft_folder = '/Users/gabimelo/Documents/MATLAB/Fieldtrip/';
+at_usp = true;
 
-% main_folder = '/Users/Gabi/Documents/GitHub/sacsamp-analysis/';
-% data_folder = 'F:/SACSAMP/';
-% ft_folder = '/Users/Gabi/Documents/MATLAB/Fieldtrip/';
+if at_usp
+    main_folder = '/Users/Gabi/Documents/GitHub/sacsamp-analysis/';
+    data_folder = 'F:/SACSAMP/';
+    ft_folder = '/Users/Gabi/Documents/MATLAB/Fieldtrip/';
+else
+    main_folder = '/Users/gabimelo/Documents/GitHub/sacsamp-analysis/';
+    data_folder = '/Volumes/PortableSSD/SACSAMP/';
+    ft_folder = '/Users/gabimelo/Documents/MATLAB/Fieldtrip/';
+end
 
 addpath(genpath(main_folder))
 cd(main_folder)
@@ -20,17 +24,16 @@ ft_defaults
 %%% load data
 
 sub = 4;       % 4 | 18 | 22
-block = 12;
+block = 6;
 
 if sub < 10
     filedir = sprintf('%ssacsamp0%i_s0%i/', data_folder, sub, sub);
 else
-    filedir = sprintf('%ssacsamp0%i_s%i/', data_folder, sub, sub);
+    filedir = sprintf('%ssacsamp%i_s%i/', data_folder, sub, sub);
 end
 
 filename = sprintf('dat_prep_b%i.mat', block);
-filepath = [filedir filename];
-load(filepath, 'dat')
+load([filedir filename], 'dat')
 dat_prep = dat;
 
 
@@ -41,27 +44,29 @@ cfg.viewmode = 'vertical';
 cfg.continuous = 'no';
 cfg.layout = 'neuromag306all.lay';
 cfg.allowoverlap = 'yes';
-cfg.verticalpadding = 0.1;
-
-% cfg.channel = {'EYEH','EYEV'};
-cfg.channel = {'EOGH','EOGV','EYEH','EYEV'};
-cfg.chanscale = [400,400,0.5,0.5];
+cfg.verticalpadding = 0.3;
+cfg.channel = {'EYEH','EYEV'};      % {'EOGH','EOGV','EYEH','EYEV'};
+cfg.chanscale = [0.3,0.3];          % [400,400,0.5,0.5];
 
 cfg = ft_databrowser(cfg, dat_prep);
-cfg_artfctdef = cfg.artfctdef;
+artifact_times = cfg.artfctdef.visual;
 
 
-%% 
-
-%%% remove artifacts and save data
+%%% remove artifacts
 
 cfg = [];
-cfg.artifactdef = cfg_artfctdef;
+cfg.artfctdef.visual = artifact_times;
+cfg.artfctdef.reject = 'nan';               % fill rejected trials with nans
 dat_clean = ft_rejectartifact(cfg, dat_prep);
+
+
+%%% save data
 
 dat = dat_clean;
 filename = sprintf('dat_clean_b%i.mat', block);
-filepath = [filedir filename];
-save(filepath,'dat')
 
-
+if ~exist([filedir filename],'file')
+    save([filedir filename],'dat')
+else
+    error('file already exists - not overwritting')
+end
--- MEG/preprocessing.m
+++ MEG/preprocessing.m
@@ -1,12 +1,16 @@
 clear all; clc;
 
-main_folder = '/Users/gabimelo/Documents/GitHub/sacsamp-analysis/';
-data_folder = '/Volumes/PortableSSD/SACSAMP/';
-ft_folder = '/Users/gabimelo/Documents/MATLAB/Fieldtrip/';
+at_usp = true;
 
-% main_folder = '/Users/Gabi/Documents/GitHub/sacsamp-analysis/';
-% data_folder = 'F:/SACSAMP/';
-% ft_folder = '/Users/Gabi/Documents/MATLAB/Fieldtrip/';
+if at_usp
+    main_folder = '/Users/Gabi/Documents/GitHub/sacsamp-analysis/';
+    data_folder = 'F:/SACSAMP/';
+    ft_folder = '/Users/Gabi/Documents/MATLAB/Fieldtrip/';
+else
+    main_folder = '/Users/gabimelo/Documents/GitHub/sacsamp-analysis/';
+    data_folder = '/Volumes/PortableSSD/SACSAMP/';
+    ft_folder = '/Users/gabimelo/Documents/MATLAB/Fieldtrip/';
+end
 
 addpath(genpath(main_folder))
 cd(main_folder)
@@ -14,6 +18,11 @@ cd(main_folder)
 addpath(ft_folder)
 ft_defaults
 
+load('full_data.mat','data')
+task_info = data;
+clear data
+
+sub_n = [10 9 13 4 17 21 11 1 22 25 26 19 18 20 28 34 23 27 49 42 35 45 36 48 16 29 38 30];
 
 % trigger labels
 % act|pas|fix
@@ -25,7 +34,6 @@ ft_defaults
 % 32 |33 |34 - response offset
 % 41:52 - array onset
 % 81:92 - target onset
-
 
 % blocks num
 % act|pas|fix
@@ -39,7 +47,7 @@ ft_defaults
 
 %%% load data
 
-sub = 22;       % 4 | 18 | 22
+sub = 4;       % 4 | 18 | 22
 block = 12;
 
 if sub < 10
@@ -53,19 +61,42 @@ if block < 10
 else
     filename = sprintf('run%i_sss.fif', block);
 end
-filepath = [filedir filename];
 
 
 %%% define trials based on triggers
 
 cfg = [];
-cfg.dataset = filepath;
+cfg.dataset = [filedir filename];
 cfg.trialdef.eventtype = 'STI101';
 cfg.trialdef.eventvalue = 81:92;      % target onset
 cfg.trialdef.prestim = 0.2;
 cfg.trialdef.poststim = 0.3;
 
 cfg = ft_definetrial(cfg);
+
+
+%%% add trial info to cfg.trl
+
+trl_num = task_info.trl_num(task_info.sub_num == sub_n(sub) & task_info.blc_num==block);
+targ_len = task_info.targ_len(task_info.sub_num == sub_n(sub) & task_info.blc_num==block);
+targ_num = task_info.targ_num(task_info.sub_num == sub_n(sub) & task_info.blc_num==block);
+
+% deal with trigger duplicates
+if length(cfg.trl) > 216
+    r = find((diff(cfg.trl(:,4))>1) | (diff(cfg.trl(:,4))==0));
+    for i = 1:numel(r)
+        trl_num = trl_num([1:r(i) nan(1,1) r(i)-1:end]);
+        targ_len = targ_len([1:r(i) nan(1,1) r(i)-1:end]);
+        targ_num = targ_num([1:r(i) nan(1,1) r(i)-1:end]);
+    end
+end
+
+cfg.trl(:,5) = trl_num;
+cfg.trl(:,6) = targ_len;
+cfg.trl(:,7) = targ_num;
+
+
+%%% preprocess data
 
 dat_preproc = ft_preprocessing(cfg);
 
@@ -78,7 +109,6 @@ else
     chans = {'all', '-EXCI', '-IASX+', '-IASX-', '-IASY+', '-IASY-', '-IASZ+', '-IASZ-', '-IAS_DX', '-IAS_DY', '-IAS_X', '-IAS_Y', '-IAS_Z', '-SYS201'};
 end
 
-cfg = [];
 cfg.channel = ft_channelselection(chans, dat_preproc.label);
 dat_select = ft_selectdata(cfg, dat_preproc);
 
@@ -95,82 +125,22 @@ dat_select.label(313) = {'EYEV'};
 dat_select.label(314) = {'PUPIL'};
 
 
+if length(cfg.trl) > 216
+    fprintf('\n\n found %i trigger duplicates \n', numel(r))
+end
+
+
 %%% save data
 
 dat = dat_select;
 filename = sprintf('dat_prep_b%i.mat', block);
-filepath = [filedir filename];
-save(filepath, 'dat')
+save([filedir filename], 'dat')
 
+trl = cfg.trl;
+filename = sprintf('trl_b%i.mat', block);
+save([filedir filename], 'trl')
 
-
-
-
-
-
-%%
-
-
-
-sub = 4;       % 4 | 18 | 22
-block = 4;
-
-if sub < 10
-    filedir = sprintf('%ssacsamp0%i_s0%i/', data_folder, sub, sub);
-else
-    filedir = sprintf('%ssacsamp%i_s%i/', data_folder, sub, sub);
-end
-
-if block < 10
-    filename = sprintf('run0%i_sss.fif', block);
-else
-    filename = sprintf('run%i_sss.fif', block);
-end
-filepath = [filedir filename];
-
-
-cfg = [];
-cfg.dataset = filepath;
-dat_preproc = ft_preprocessing(cfg);
-
-if sub <= 9
-    chans = {'all', '-IASX+', '-IASX-', '-IASY+', '-IASY-', '-IASZ+', '-IASZ-', '-IAS_DX', '-IAS_DY', '-IAS_X', '-IAS_Y', '-IAS_Z', '-SYS201'};
-else
-    chans = {'all', '-EXCI', '-IASX+', '-IASX-', '-IASY+', '-IASY-', '-IASZ+', '-IASZ-', '-IAS_DX', '-IAS_DY', '-IAS_X', '-IAS_Y', '-IAS_Z', '-SYS201'};
-end
-
-cfg = [];
-cfg.channel = ft_channelselection(chans, dat_preproc.label);
-dat_select = ft_selectdata(cfg, dat_preproc);
-
-dat_select.label(1) = {'EOGH'};
-dat_select.label(2) = {'EOGV'};
-% dat_select.label(3) = {'ECG'};
-% dat_select.label(310) = {'EMG1'};
-% dat_select.label(311) = {'EMG2'};
-dat_select.label(312) = {'EYEH'};
-dat_select.label(313) = {'EYEV'};
-dat_select.label(314) = {'PUPIL'};
-
-%%
-cfg = [];
-cfg.viewmode = 'vertical';
-cfg.continuous = 'yes'; 
-cfg.layout = 'neuromag306all.lay';
-cfg.allowoverlap = 'yes';
-cfg.plotevents = 'yes';
-cfg.blocksize = 50;
-cfg.verticalpadding = 0.1;
-
-% cfg.channel = {'EOGH','EOGV'};
-cfg.channel = {'EOGH','EOGV','EYEH','EYEV'};
-cfg.chanscale = [600,600,0.5,0.5];
-
-ft_databrowser(cfg, dat_select);
-
-%%
-
-figure()
-chan = find(strcmp(dat_select.label, 'EYEV'));
-plot(dat_select.time{1}, dat_select.trial{1}(chan,:))
+evt = cfg.event;
+filename = sprintf('evt_b%i.mat', block);
+save([filedir filename], 'evt')
 
